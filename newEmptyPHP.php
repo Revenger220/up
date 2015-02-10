dcc19f051bf3218be42567c42ff07a75e28da89a
Revenger220 <maksim_korneev@ukr.net>
2015-02-11 00:00:28 +0200

    up.vladke

---
+++ newEmptyPHP.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Новый проэкт</title>
+    </head>
+
+
+    <body>
+
+
+        <?php
+        ?>
+
+
+
+
+    </body>
+</html>
